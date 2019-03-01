04c5b50a02c4f2f452097d590f215279427be3c3
Serrano Pertuz <000404139@bga.upb>
2019-03-01 12:40:34 -0500

    Mi primer commit

---
+++ mayor.py
@@ -0,0 +1,9 @@
+a=float(input("Numero Deseado a comparar: "))
+b=int(a)
+if b>0:
+    print("El numero entero: ", b, "es mayor a 0")
+else:
+ if b==0:
+     print("El numero entero: ", b, "es igual a 0")
+ else:
+     print("El numero entero: " ,b, "Es menor a 0" )
