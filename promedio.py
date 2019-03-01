fbfb3f5be79172591716120e7a300c6162fd4911
valquero779 <47458199+valquero779@users.noreply.github.com>
2019-03-01 14:58:03 -0500

    Edicion

--- promedio.py
+++ promedio.py
@@ -1,6 +1,14 @@
-a=0
-b=0
-while a>=0 and b>=0 and a<500 and b<500:
-  print("La suma es: " ,a+b, "el promedio es: " ,(a+b)/2)
-  a+=1
-  b+=1
+a = 0
+b = 0
+for x in range(0, 50090900):
+    a=int(input("numero"))
+    b=int(input("numero"))
+    if a > 0 and b > 0:
+        print((a + b))
+        a += 1
+        b += 1
+    else:
+        if a > 0 and b > 0:
+            print((a + b) / 2)
+            a += 1
+            b += 1
